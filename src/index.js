343fff05e8584e3305d64fd073be3a89e46e3e2d
miker-mcd <michaelrmcd@gmail.com>
2018-04-30 22:32:47 -0700

    Update SearchBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyC9WFNCt4TappeALREK-p3FippwcuzItuI';
+
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 // Take this component's generated HTML and put it on
