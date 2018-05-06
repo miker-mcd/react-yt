9cda175910e5fe298daa7c94c9ef5d66b0860cf7
miker-mcd <michaelrmcd@gmail.com>
2018-05-05 22:11:30 -0700

    Update VideoList and VideoListItem components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyC9WFNCt4TappeALREK-p3FippwcuzItuI';
 
 // Create a new component. This component should produce
 // some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 // Take this component's generated HTML and put it on
 // the page (in the DOM)
